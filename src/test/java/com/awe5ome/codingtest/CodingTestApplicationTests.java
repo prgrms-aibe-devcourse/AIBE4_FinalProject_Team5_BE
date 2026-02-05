781d134199bf5bf0769ee05e523d3a242a6826c5
tmddn7475 <junseungu74@naver.com>
2026-02-06 08:45:29 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/test/java/com/awe5ome/codingtest/CodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awe5ome.codingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
