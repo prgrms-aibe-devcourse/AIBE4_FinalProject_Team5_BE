05065b31b8d92c83ab13c2b4c21669e172cbbeff
chomeco <chomeco99@gmail.com>
2026-02-10 10:34:39 +0900

    Project Default Settings

---
+++ src/main/java/kr/java/coditor/CoditorApplication.java
@@ -0,0 +1,13 @@
+package kr.java.coditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoditorApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kr/java/coditor/global/controller/HealthController.java
@@ -0,0 +1,12 @@
+package kr.java.coditor.global.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/api/health")
+    public String healthCheck(){
+        return "i'm still alive";
+    }
+}
