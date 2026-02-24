b51c5c8c6dd91c4d634a025c8db4cdc8cc533c0a
audq0 <epqmzhtm0909@gmail.com>
2026-02-24 15:15:07 +0900

    feat : user entity 작성

---
+++ src/main/java/kr/java/coditor/domain/user/entity/User.java
@@ -0,0 +1,53 @@
+package kr.java.coditor.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "nickname", length = 50, nullable = false)
+	private String nickname;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role")
+	private Role role = Role.USER;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "provider")
+	private Provider provider = Provider.LOCAL;
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	@Builder
+	public User(String email, String password, String nickname, Role role, Provider provider) {
+		this.email = email;
+		this.password = password;
+		this.nickname = nickname;
+		this.role = role != null ? role : Role.USER;
+		this.provider = provider != null ? provider : Provider.LOCAL;
+	}
+}
